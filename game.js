0917c0189693fcda4953e5a7d90fd305ad2d03f8
tacnyan <giovannalimapereira484@gmail.com>
2026-05-08 14:21:30 -0300

    up

--- game.js
+++ game.js
@@ -1,57 +1,81 @@
 
 // "}" is a block code, there is, beginning and end
-const quiz = [
-  {
-    pergunta: "você recebeu um email suspeito, o que você faria?",
-    opcoes: ["nao sei", "responderia", "clasificaria como spam","ignorava", "mandaria no grupo do zap", "denunciaria"],
-    correta: 2
-  },
-  {
-    pergunta:"você acaba de entrar em um site, mas derrepente começa \n a aparecer anuncios sem parar, o quê fazer?",
-    opcoes: ["chorar", "sair do site e não aceitar nenhum cookie", "clicar em todos os anuncios e comprar tudo kkkk"],
-    correta: 1
+// const quiz = [
+//   {
+//     pergunta: "você recebeu um email suspeito, o que você faria?",
+//     opcoes: ["nao sei", "responderia", "clasificaria como spam","ignorava", "mandaria no grupo do zap", "denunciaria"],
+//     correta: 2
+//   },
+//   {
+//     pergunta:"você acaba de entrar em um site, mas derrepente começa \n a aparecer anuncios sem parar, o quê fazer?",
+//     opcoes: ["chorar", "sair do site e não aceitar nenhum cookie", "clicar em todos os anuncios e comprar tudo kkkk"],
+//     correta: 1
     
-  }
-];
+//   }
+// ];
 
-let indiceAtual = 0;
-let pontuacao = 0;
+// let indiceAtual = 0;
+// let pontuacao = 0;
 
-//como mostrar pergunta/mostrar text visivel em html
-function mostrarPergunta() {
-  const pergunta = quiz[indiceAtual];
-  document.getElementById("pergunta").innerText = pergunta.pergunta;
+// //como mostrar pergunta/mostrar text visivel em html
+// function mostrarPergunta() {
+//   const pergunta = quiz[indiceAtual];
+//   document.getElementById("pergunta").innerText = pergunta.pergunta;
 
-  const respostasDiv = document.getElementById("respostas");
-  respostasDiv.innerHTML = "";
+//   const respostasDiv = document.getElementById("respostas");
+//   respostasDiv.innerHTML = "";
 
-//   Desenha no site cada opção da pergunta
-  pergunta.opcoes.forEach((opcao, index) => {
-    const botao = document.createElement("button");
-    botao.innerText = opcao;
-    botao.onclick = () => verificarResposta(index);
-    respostasDiv.appendChild(botao);
-  });
+// //   Desenha no site cada opção da pergunta
+//   pergunta.opcoes.forEach((opcao, index) => {
+//     const botao = document.createElement("button");
+//     botao.innerText = opcao;
+//     botao.onclick = () => verificarResposta(index);
+//     respostasDiv.appendChild(botao);
+//   });
+// }
+
+// function verificarResposta(index) {
+//   if (index === quiz[indiceAtual].correta) {
+//     pontuacao++;
+//   }
+// }
+
+// function proximaPergunta() {
+//   indiceAtual++;
+
+//   if (indiceAtual < quiz.length) {
+//     mostrarPergunta();
+//   } else {
+//     document.getElementById("resultado").innerText =
+//       "Pontuação: " + pontuacao;
+//   }
+// }
+
+
+// mostrarPergunta();
+
+let shinji = `aqui voce ira responder algumas perguntas sobre segurança digital` 
+let asuka = `aqui vc vera a tragedia que é a vida da asuka kkakakakakka`
+let rei = `triste mae do shinji`
+
+function mostrarh(personagem) {
+
+let historia = document.getElementById("historia");
+{
+
+if (personagem === 'shinji') {
+
+historia.innerHTML = shinji
+}
+  
+else if (personagem === 'rei') {
+
+ historia.innerHTML = rei
+}
+else {
+  historia.innerHTML = asuka
+}
+}
 }
 
-function verificarResposta(index) {
-  if (index === quiz[indiceAtual].correta) {
-    pontuacao++;
-  }
-}
-
-function proximaPergunta() {
-  indiceAtual++;
-
-  if (indiceAtual < quiz.length) {
-    mostrarPergunta();
-  } else {
-    document.getElementById("resultado").innerText =
-      "Pontuação: " + pontuacao;
-  }
-}
-
-
-//document.body.style.textAlign = "right";
-
-mostrarPergunta();
+// document.body.style.textAlign = "left";
